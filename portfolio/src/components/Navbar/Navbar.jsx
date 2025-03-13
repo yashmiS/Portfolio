8840ac1cc213ea9c78f1aebbd5fe8bc4b59fa0ce
yashmiS <savindiabeywickrama@gmail.com>
2025-03-13 22:54:32 +0530

    Modify Navbar

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,41 @@
-import React, {useState} from 'react'
-import styles from './Navbar.module.css'
-import { getImageUrl} from '../../utils'
+import React, { useState } from "react";
+
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
-        <a className={styles.title} href='/'>Portfolio</a>
-        <div className={styles.menu}>
-          <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt='menu-button'/>
-          <ul className={styles.menuItems}>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#experience'>Experience</a></li>
-            <li><a href='#projects'>Projects</a></li>
-            <li><a href='#contact'>Contact</a></li>
-          </ul>
-        </div>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={menuOpen ? "/assets/nav/closeIcon.png" : "/assets/nav/menuIcon.png"} 
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
-  )
-  
-}
+  );
+};
