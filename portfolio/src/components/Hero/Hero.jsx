67b7ce56f339436f821cb16181d0e31f7c80a1df
yashmiS <savindiabeywickrama@gmail.com>
2025-03-14 23:52:42 +0530

    Add description to hero component

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { getImageUrl } from '../../utils'
 
 function Hero() {
-  return (
-    <div>Hero</div>
-  )
+  return <section>
+    <div>
+    <h1>Hi, I'm Yashmi</h1>
+    <p>I'm Fullstack Developer with 2 years experince using Angular and .Net.Reach out If you like to know about more!</p>
+    <a href='savindiabeywickrama@gmail.com'>Contact Me</a>
+    </div>
+    <img src = "/assets/hero/heroImage.png"/>
+  </section>
 }
 
 export default Hero
