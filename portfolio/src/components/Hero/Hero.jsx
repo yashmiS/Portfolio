340f8335b9796a100477bfd24a80edceccbc7d55
yashmiS <savindiabeywickrama@gmail.com>
2025-03-19 22:02:39 +0530

    add css classes

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { getImageUrl } from '../../utils'
+import styles from './Hero.module.css'
 
 function Hero() {
-  return <section>
-    <div>
-    <h1>Hi, I'm Yashmi</h1>
-    <p>I'm Fullstack Developer with 2 years experince using Angular and .Net.Reach out If you like to know about more!</p>
-    <a href='savindiabeywickrama@gmail.com'>Contact Me</a>
+  return <section className={styles.container}>
+    <div className={styles.content}>
+    <h1 className={styles.title}>Hi, I'm Yashmi</h1>
+    <p className={styles.description}>I'm Fullstack Developer with 2 years experince using Angular and .Net.Reach out If you like to know about more!</p>
+    <a href='savindiabeywickrama@gmail.com' className={styles.contactBtn}>Contact Me</a>
     </div>
-    <img src = "/assets/hero/heroImage.png"/>
+    <img src = "/assets/hero/heroImage.png" alt='Hero image of me ' className={styles.heroImg}/>
+    <div className={styles.topBlur}/>
+    <div className={styles.bottomBlur}/>
   </section>
 }
 
