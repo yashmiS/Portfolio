3d10495f1637d8624b2a938e20fde228b834fe64
yashmiS <savindiabeywickrama@gmail.com>
2025-04-05 22:08:41 +0530

    Modify about page

---
+++ portfolio/src/components/About/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function About() {
+  return (
+    <section>
+        <h2>About</h2>
+        <div><img src='assets/about/aboutImage.png' alt='Me sitting with a laptop'/>
+        <ul>
+            <li>
+                <img src='' alt=''/>
+            </li>
+        </ul>
+        </div>
+    </section>
+  )
+}
+
+export default About
