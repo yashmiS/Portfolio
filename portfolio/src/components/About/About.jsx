a5f39c8e3d2a2b5740221d68da188458e3224233
yashmiS <savindiabeywickrama@gmail.com>
2025-04-14 21:26:39 +0530

    add styles

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -4,7 +4,7 @@ import styles from './About.module.css'
 
 function About() {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id='about'>
         <h2 className={title}>About</h2>
         <div className={styles.content}>
           <img src='assets/about/aboutImage.png' alt='Me sitting with a laptop'
@@ -12,21 +12,21 @@ function About() {
         <ul className={styles.aboutItems}>
             <li className={styles.aboutImage}>
                 <img src='assets/about/cursorIcon.png' alt='Cursor'/>
-                <div>
+                <div className={styles.aboutItemText}>
                   <h3>Frontend Developer</h3>
                   <p>I'm a frontend developer with experience in building responsive and optimized sites.</p>
                 </div>
             </li>
             <li className={styles.aboutImage}>
                 <img src='assets/about/serverIcon.png' alt='Server'/>
-                <div>
+                <div className={styles.aboutItemText}>
                   <h3>Backend Developer</h3>
                   <p>I have experience developing fast abd optimised back-end systems and APIs.</p>
                 </div>
             </li>
             <li className={styles.aboutImage}>
                 <img src='assets/about/uiIcon.png' alt='UI'/>
-                <div>
+                <div className={styles.aboutItemText}>
                   <h3>UI Designer</h3>
                   <p>I have desinged multiple landing pages and have systems as well.</p>
                 </div>
