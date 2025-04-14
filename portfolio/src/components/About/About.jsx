733de0457020ca5dc409e4d7845dfa55a7dc1189
yashmiS <savindiabeywickrama@gmail.com>
2025-04-14 06:56:42 +0530

    add details in about page

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import styles from './About.module.css'
+
 
 function About() {
   return (
-    <section>
-        <h2>About</h2>
-        <div><img src='assets/about/aboutImage.png' alt='Me sitting with a laptop'/>
-        <ul>
-            <li>
-                <img src='' alt=''/>
+    <section className={styles.container}>
+        <h2 className={title}>About</h2>
+        <div className={styles.content}>
+          <img src='assets/about/aboutImage.png' alt='Me sitting with a laptop'
+          className={styles.aboutImage}/>
+        <ul className={styles.aboutItems}>
+            <li className={styles.aboutImage}>
+                <img src='assets/about/cursorIcon.png' alt='Cursor'/>
+                <div>
+                  <h3>Frontend Developer</h3>
+                  <p>I'm a frontend developer with experience in building responsive and optimized sites.</p>
+                </div>
+            </li>
+            <li className={styles.aboutImage}>
+                <img src='assets/about/serverIcon.png' alt='Server'/>
+                <div>
+                  <h3>Backend Developer</h3>
+                  <p>I have experience developing fast abd optimised back-end systems and APIs.</p>
+                </div>
+            </li>
+            <li className={styles.aboutImage}>
+                <img src='assets/about/serverIcon.png' alt='Server'/>
+                <div>
+                  <h3>UI Designer</h3>
+                  <p>I have desinged multiple landing pages and have systems as well.</p>
+                </div>
             </li>
         </ul>
         </div>
